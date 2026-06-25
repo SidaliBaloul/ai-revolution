a96cd445dccc524dbeab4f7db2de5ee7dbe24dfe
Sidali <Sidali@DESKTOP-G4IJ8NB>
2026-06-25 17:57:38 +0100

    Add heroSection

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -1,9 +1,68 @@
+import Image from "next/image";
 
+const images = [
+  "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=600&auto=format&fit=crop&q=60",
+  "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=600&auto=format&fit=crop&q=60",
+  "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60",
+];
 
-export default function HeroSection(){
-   return (
-    <div>
-        heroSection
-    </div>
-   );
+export default function HeroSection() {
+  return (
+    <section
+      id="section"
+      className="app-container pt-24 pb-12 overflow-hidden sm:overflow-visible"
+    >
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+            <span className="gradient-text">Artificial Antiligence</span>
+            <br />
+            for a smarter future
+          </h1>
+          <p className="text-lg text-gray-600 max-w-lg">
+            Discover how AI is transforming industries and improving lives world
+            wide. Our cutting-edge solutions harness the power of machine
+            learning to solve complex problems.
+          </p>
+
+          <div className="flex flex-col sm:flex-row  gap-4 pt-4">
+            <a className="btn-primary" href="#">
+              Explore AI Solutions
+            </a>
+            <a className="btn-outline" href="#">
+              Learn More
+            </a>
+          </div>
+
+          <div className="pt-4 flex items-center space-x-4">
+            <div className="flex -space-x-2">
+              {images.map((img, i) => (
+                <img
+                  className="avatar"
+                  key={i}
+                  src={img}
+                  alt={`User ${i + 1}`}
+                />
+              ))}
+            </div>
+
+            <p className="text-sm font-medium text-gray-600">
+              Trusted by
+              <span className="text-primary font-bold">+10000</span> people
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full h-80 md:h-150">
+          <img
+            src="https://images.unsplash.com/photo-1674027444485-cec3da58eef4?q=80&w=1932&auto=format&fit=crop"
+            alt="hero section"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute w-32 h-32 rounded-full bg-secondary/20 -bottom-6 -left-6 blur-xl"></div>
+          <div className="absolute w-32 h-32 rounded-full bg-accent/20 -tp-6 -right-6 blur-xl"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
